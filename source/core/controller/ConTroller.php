b93dd3d94c9b9b07ae312c1be059a801059c8f07
Patobeur41 <48214878+patobeur@users.noreply.github.com>
2020-03-30 15:23:32 +0200

    add page accueil parske c'est cool !

---
+++ source/core/controller/ConTroller.php
@@ -0,0 +1,21 @@
+<?php
+    $coucou = 'coucou';
+?>
+<?='<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<pre>
+    projet : <br/>
+    objectifs : <br/>
+    date début : <br/>
+    date fin : <br/>
+    équipe  : <br/>
+    apps : <br/>
+</pre>    
+</body>
+</html>'?>
